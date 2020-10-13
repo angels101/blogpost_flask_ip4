a5f315d3044ae5382d54f90a4ae524889a372fdb
angels101 <angelscodex101@gmail.com>
2020-10-13 07:58:44 +0300

    database configuration

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+class Config:
+    
+    SECRET_KEY = 'adds'
+    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://arthur:***@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+   
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+} 
